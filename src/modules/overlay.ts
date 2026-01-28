7a4e75f4ec6d3305c298b3c19a014480468d102d
Dom <dominicd7@hotmail.com>
2026-01-28 01:20:45 +0100

    lint

--- src/modules/overlay.ts
+++ src/modules/overlay.ts
@@ -891,7 +891,7 @@ export default class ZoteroReadingList {
 		progress.setProgress(0);
 		progressWindow.show();
 		try {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+			 
 			await Zotero.DB.executeTransaction(() => {
 				for (const item of allItems) {
 					const readStatusTags = this.getItemReadStatusTags(item);
@@ -939,7 +939,7 @@ export default class ZoteroReadingList {
 		try {
 			progress.setProgress(0);
 			progressWindow.show();
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+			 
 			await Zotero.DB.executeTransaction(() => {
 				for (const item of allItems) {
 					const itemReadStatus = this.getItemReadStatus(item);
